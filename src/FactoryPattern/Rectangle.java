192d7a94fcec03fbe55105f46762cf2e47c8aed8
Mansi Pattani <mansipattani69@gmail.com>
2016-01-22 18:23:48 +0530

    Added examples of important design patterns

---
+++ src/FactoryPattern/Rectangle.java
@@ -0,0 +1,11 @@
+package FactoryPattern;
+
+/**
+ * Created by mansi.pattani on 22/1/16.
+ */
+public class Rectangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
---
+++ src/FactoryPattern/Square.java
@@ -0,0 +1,11 @@
+package FactoryPattern;
+
+/**
+ * Created by mansi.pattani on 22/1/16.
+ */
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
